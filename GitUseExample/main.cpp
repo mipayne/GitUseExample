950a92f2420d6c11630a24a56bbbf27dbb54ae70
Madelyn Payne <mpayne@mit.edu>
2017-06-08 15:49:34 -0400

    print grid from state vector

--- GitUseExample/main.cpp
+++ GitUseExample/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    std::vector<char> type_vector;
+    int side_length = 4;
+    type_vector.reserve(side_length*side_length);
+    for(int j = side_length; j > 0; --j)
+    {
+        for (int i = 0; i < side_length; ++i)
+        {
+            if( i < side_length/2)
+            {
+                type_vector.push_back('A');
+            }
+            else
+            {
+                type_vector.push_back('B');
+            }
+        }
+    }
+    for(int i = 0; i < type_vector.capacity(); ++i)
+    {
+        std::cout<<type_vector[i]<< ' ';
+        if( (i+1)%side_length ==0)
+        {
+            std::cout<< ' '<<std::endl;
+        }
+    }
     return 0;
 }
