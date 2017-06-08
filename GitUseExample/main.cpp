17f7bd931f77d6c2384edfe2de7b6e77d5196e47
Madelyn Payne <mpayne@mit.edu>
2017-06-08 15:07:45 -0400

    Initial Commit

---
+++ GitUseExample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitUseExample
+//
+//  Created by Madelyn Payne on 6/8/17.
+//  Copyright © 2017 Madelyn Payne. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
